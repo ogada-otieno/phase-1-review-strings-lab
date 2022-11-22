dee652441fedc396817b8fbd5672e1d435c81c69
Rodgers Ogada <ogadarodgers@gmail.com>
2022-11-22 08:58:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+const currentUser = 'Sankara Jade';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toLocaleUpperCase();
+// const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
